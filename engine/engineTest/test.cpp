d5e6fffcb08e8f94df459e33049c0d933741e0cc
Ryan McCoy <rrmccoy@mtu.edu>
2019-02-27 18:39:58 -0500

    added second test currently empty.

--- engine/engineTest/test.cpp
+++ engine/engineTest/test.cpp
@@ -1,6 +1,6 @@
 #include "pch.h"
 
-TEST(physicalObjectTest, deaultConstructor) {
+TEST(physicalObjectTest, defaultConstructor) {
 	physicalObject obj;
 	EXPECT_EQ(obj.getXpos(), 0);
 	EXPECT_EQ(obj.getYpos(), 0);
@@ -8,4 +8,8 @@ TEST(physicalObjectTest, deaultConstructor) {
 	EXPECT_EQ(obj.getYvel(), 0);
 	EXPECT_EQ(obj.getXacc(), 0);
 	EXPECT_EQ(obj.getYacc(), 0);
+}
+
+TEST(physicalObjectTest, constructor) {
+	//physicalObject obj((float) 5, (float) 5);
 }
