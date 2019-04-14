4dc636b984a3eaec11d079cb7d8eb94f22230002
ajmagnus <45823834+ajmagnus@users.noreply.github.com>
2019-04-14 15:13:02 -0400

    ForceDemo: Adjusted mass of circle and amount of force applied

--- game/ForceDemo/ForceDemo.cpp
+++ game/ForceDemo/ForceDemo.cpp
@@ -24,7 +24,7 @@ int main()
 	circle o1 = circle(100.f);
 	o1.setVelocity(vector2(0, 0));
 	o1.setAcceleration(vector2(0, 0));
-	o1.setMass(1000.f);
+	o1.setMass(5.f);
 
 	physics.addCircle(o1);
 
@@ -72,22 +72,22 @@ int main()
 		// Applying force to the circle when wasd is pressed
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
 		{
-			force downForce = force(&o1, vector2(0, 960));
+			force downForce = force(&o1, vector2(0, 600));
 		}
 
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
 		{
-			force upForce = force(&o1, vector2(0, -960));
+			force upForce = force(&o1, vector2(0, -600));
 		}
 
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
 		{
-			force downForce = force(&o1, vector2(-960, 0));
+			force downForce = force(&o1, vector2(-600, 0));
 		}
 
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
 		{
-			force downForce = force(&o1, vector2(960, 0));
+			force downForce = force(&o1, vector2(600, 0));
 		}
 
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
